5ca40c784caeeb2391cb8ae0c23b2adb15d59005
Pritish <52450143+PMK4095@users.noreply.github.com>
2025-05-12 13:39:32 +0530

    Add files via upload
    done with the first assignment.

---
+++ tasK_2.py
@@ -0,0 +1,4 @@
+#Task 2
+first_name=input(str("Enter your first name: "))
+last_name=input(str("Enter your last name: "))
+print("Hello, " + first_name + " " + last_name + "! Welcome to the Python program.")
---
+++ task_1.py
@@ -0,0 +1,13 @@
+#Task 1
+number_1=int(input("Enter the first number: "))
+number_2=int(input("Enter the second number: "))
+
+add=number_1+number_2
+sub=number_1-number_2
+mul=number_1*number_2
+div=number_1/number_2
+
+print("Addition :",add)
+print("Subtraction :",sub)
+print("Multiplication :",mul)
+print("Division :",div)
